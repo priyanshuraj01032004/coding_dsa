6adebc6683049c16fef66d9b59827eb22c628d8e
PriyanshuRajvishal103 <priyanshuraj01032004@gmail.com>
2025-03-04 09:56:47 +0530

    update

--- max_consecutive_ones.cpp
+++ max_consecutive_ones.cpp
@@ -4,10 +4,15 @@ using namespace std;
 int main(){
     int arr[] = {1,2,1,1,2,1,1,1,1,2,1,1,1,1,1};
     int size = sizeof(arr)/sizeof(arr[0]);
-
-    int hash[100] = {};
-    for(int i=0; i< 1000;i++){
-        hash[arr[i]]
+    int max_count = 0,count = 0;
+    for(int i=0; i< size;i++){
+        if(arr[i] == 1){
+            count++;
+            max_count = max(max_count,count);
+        }
+        else{
+            count = 0;
+        }
     }
 
     cout << "the maximum number of cosecutive 1's are "<< max_count;
